8f489a19e6e206d94f9301ce4e483f75f81a4c1d
Qing Liu <37494307+toutou826@users.noreply.github.com>
2019-12-13 00:46:00 -0500

    error handling

--- app/routes.py
+++ app/routes.py
@@ -68,7 +68,7 @@ def user(username):
         {'author':user, 'body':'Test post #1'},
         {'author':user, 'body':'Test post #2'}
     ]
-    return render_template('user.html', user=user, posts=posts)
+    return render_template('user.html', user=user, title='profile', posts=posts)
 
 @app.before_request
 def before_request():
@@ -79,7 +79,7 @@ def before_request():
 @app.route('/edit_profile', methods=['GET','POST'])
 @login_required
 def edit_profile():
-    form = EditProfileForm()
+    form = EditProfileForm(current_user.username)
     if form.validate_on_submit():
         current_user.username = form.username.data
         current_user.about_me = form.about_me.data
